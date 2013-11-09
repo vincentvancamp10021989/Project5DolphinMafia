156e39417dad73332587ae089b5f4ec91138d025
Jim <jim.vansteenwegen@student.ap.be>
2013-11-09 18:59:05 +0100

    jim versie 0.1.2 logout 100%

---
+++ WebApplication1/Logout.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebApplication1.Klasses;
+
+namespace WebApplication1
+{
+    public partial class Logout : System.Web.UI.Page
+    {
+        private const string NEXT_PAGE = "LoginView.aspx";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            HttpContext.Current.Session[SessionEnum.SessionNames.LecturorsID.ToString()] = string.Empty;
+            HttpContext.Current.Response.Redirect(NEXT_PAGE);
+        }
+    }
+}
